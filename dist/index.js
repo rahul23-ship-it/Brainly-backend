c9cf104fdce246236ccc91664825cd3334131144
Rahul <rahul.kaladharan2303@gmail.com>
2026-02-18 08:23:13 +0000

    add test

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,7 @@ import { usermiddleware } from "./middleware.js";
 import { random } from "./utils.js";
 const app = express();
 app.use(express.json());
+// test
 app.post("/api/v1/signup", async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
